5c0067c2e0f0f18cf303bf0de497b4fc216e57c5
= <=>
2022-06-11 22:29:38 -0300

    AutoLoad e namespace utilizados

---
+++ Modulo02/aula06/index.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'vendor/autoload.php';
+
+use Classes\Config\Usuario as UsuarioConfig;
+use Classes\Categoria;
+
+//$us1 = new Classes\Usuario();
+$us2 = new UsuarioConfig();
+
+$c1 = new Categoria();
+
+//var_dump($us1);
+var_dump($us2);
+
